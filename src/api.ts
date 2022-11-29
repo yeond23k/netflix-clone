4ad787e0170b06ec8589af79bc6d3e88d5a00f64
Daryl <dyk0203@naver.com>
2022-11-29 11:21:50 +0900

    6. Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = "5550e924d8b27fb43cb4c991656779d5";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMoive {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMoive[];
+  total_pages: number;
+  total_results: number;
+}
+
 export const getMovies = () => {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
