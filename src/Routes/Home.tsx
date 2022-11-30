6fb3b7fb620fad0e5f59a23b283946dde1cb70e6
Daryl <dyk0203@naver.com>
2022-12-01 01:25:45 +0900

    10. Box Animations part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -66,6 +66,19 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.innerWidth + 5,
@@ -84,9 +97,15 @@ const boxVariants = {
   },
   hover: {
     scale: 1.3,
-    y: -50,
-    transition: { delay: 0.5, duration: 0.3 },
-    type: "tween",
+    y: -80,
+    transition: { delay: 0.5, duration: 0.3, type: "tween" },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: { delay: 0.5, duration: 0.3, type: "tween" },
   },
 };
 
@@ -142,7 +161,11 @@ const Home = () => {
                       initial="normal"
                       whileHover="hover"
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                    />
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
