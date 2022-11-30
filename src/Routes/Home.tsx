f8833a547b536320a02aaa236d7951de687cc778
Daryl <dyk0203@naver.com>
2022-12-01 01:15:34 +0900

    9. Box Animations part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -58,6 +58,12 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   height: 200px;
   color: red;
   font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 
 const rowVariants = {
@@ -69,6 +75,18 @@ const rowVariants = {
   },
   exit: {
     x: -window.innerWidth - 5,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: { delay: 0.5, duration: 0.3 },
+    type: "tween",
   },
 };
 
@@ -120,6 +138,9 @@ const Home = () => {
                   .map((movie) => (
                     <Box
                       key={movie.id}
+                      variants={boxVariants}
+                      initial="normal"
+                      whileHover="hover"
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
                     />
                   ))}
